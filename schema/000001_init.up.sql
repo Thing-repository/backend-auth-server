2f57582347ab92a1a0ff01c5a422c9f3ffbaa324
emil.islamov@mircod.com <S568gKs5>
2022-06-05 21:28:25 +0300

    Fixed db migrations

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -15,18 +15,21 @@ CREATE TABLE departments
 
 CREATE TABLE users
 (
-    id                  serial primary key,
-    first_name          varchar(255)                                      not null,
-    last_name           varchar(255)                                      not null,
-    email               varchar(255)                                      not null unique,
-    image_url           varchar(255),
-    password_hash       varchar(255)                                      not null,
-    company_id          int references companies (id) on delete cascade,
-    department_id       int references departments (id) on delete cascade,
-    is_company_admin    boolean,
-    is_department_admin boolean,
-    vacation_time_start timestamp,
-    vacation_time_end   timestamp
+    id                       serial primary key,
+    first_name               varchar(255)          not null,
+    last_name                varchar(255)          not null,
+    email                    varchar(255)          not null unique,
+    image_url                varchar(255),
+    password_hash            varchar(255)          not null,
+    company_id               int references companies (id) on delete cascade,
+    department_id            int references departments (id) on delete cascade,
+    is_company_admin         boolean,
+    is_department_admin      boolean,
+    is_department_maintainer boolean,
+    vacation_time_start      timestamp,
+    vacation_time_end        timestamp,
+    email_is_validated       boolean default false not null,
+    email_validation_token   varchar(255)
 );
 
 CREATE TABLE companies_admins
@@ -38,8 +41,8 @@ CREATE TABLE companies_admins
 
 CREATE TABLE departments_admins
 (
-    id         serial primary key,
-    user_id    int references users (id) on delete cascade       not null,
+    id            serial primary key,
+    user_id       int references users (id) on delete cascade       not null,
     department_id int references departments (id) on delete cascade not null
 );
 
@@ -49,17 +52,17 @@ CREATE TYPE remainder_types as enum ('pcs', 'capacity', 'percents');
 CREATE TABLE things
 (
     id                  serial primary key,
-    thing_name          varchar(255) not null,
-    company_id int references companies (id) on delete cascade not null,
-    department_id int references departments (id) on delete cascade not null,
+    thing_name          varchar(255)                                      not null,
+    company_id          int references companies (id) on delete cascade   not null,
+    department_id       int references departments (id) on delete cascade not null,
     image_url           varchar(255),
-    thing_type          thing_types  not null,
+    thing_type          thing_types                                       not null,
     thing_remainder     real,
     remainder_type      remainder_types,
-    is_blocked          boolean      not null,
+    is_blocked          boolean                                           not null,
     blocked_time_start  timestamp,
     blocked_time_end    timestamp,
-    need_admin_approval boolean      not null
+    need_admin_approval boolean                                           not null
 );
 
 CREATE TABLE using_things
