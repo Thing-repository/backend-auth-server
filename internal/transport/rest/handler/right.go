12aa95367902b72862048bdf38949550b168405f
emil.islamov@mircod.com <S568gKs5>
2022-06-08 18:39:32 +0300

    Added right methods

--- internal/transport/rest/handler/right.go
+++ internal/transport/rest/handler/right.go
@@ -8,10 +8,28 @@ import (
 // @Summary NOT IMPLEMENTED! CompanyAdmins
 // @Security ApiKeyAuth
 // @Tags rights
+// @Description This request for get users from company admins list
+// @ID getCompanyAdmins
+// @Accept json
+// @Produces json
+// @Param companyId query int false "company id"
+// @Success 200 {array} core.CompanyManager "list of company admins"
+// @Failure 400,401,403 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /company_admins [get]
+func (H *Handler) getCompanyAdmins(c *gin.Context) {
+	newErrorResponse(c, http.StatusInternalServerError, "method not implemented")
+}
+
+// @Summary NOT IMPLEMENTED! CompanyAdmins
+// @Security ApiKeyAuth
+// @Tags rights
 // @Description This request for add user to company admins
 // @ID addCompanyAdmin
 // @Accept json
 // @Produces json
+// @Param companyId query int false "company id"
 // @Param userId query int true "added user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
@@ -29,6 +47,7 @@ func (H *Handler) addCompanyAdmin(c *gin.Context) {
 // @ID deleteCompanyAdmin
 // @Accept json
 // @Produces json
+// @Param companyId query int false "company id"
 // @Param userId query int true "deleted user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
@@ -39,6 +58,23 @@ func (H *Handler) deleteCompanyAdmin(c *gin.Context) {
 	newErrorResponse(c, http.StatusInternalServerError, "method not implemented")
 }
 
+// @Summary NOT IMPLEMENTED! DepartmentAdmins
+// @Security ApiKeyAuth
+// @Tags rights
+// @Description This request for get users from department admins list
+// @ID getDepartmentAdmins
+// @Accept json
+// @Produces json
+// @Param departmentId query int false "department id"
+// @Success 200 {array} core.DepartmentManager "list of department admins"
+// @Failure 400,401,403 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /department_admins [get]
+func (H *Handler) getDepartmentAdmins(c *gin.Context) {
+	newErrorResponse(c, http.StatusInternalServerError, "method not implemented")
+}
+
 // @Summary NOT IMPLEMENTED! DepartmentAdmin
 // @Security ApiKeyAuth
 // @Tags rights
@@ -46,6 +82,7 @@ func (H *Handler) deleteCompanyAdmin(c *gin.Context) {
 // @ID addDepartmentAdmin
 // @Accept json
 // @Produces json
+// @Param departmentId query int false "department id"
 // @Param userId query int true "added user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
@@ -63,6 +100,7 @@ func (H *Handler) addDepartmentAdmin(c *gin.Context) {
 // @ID deleteDepartmentAdmin
 // @Accept json
 // @Produces json
+// @Param departmentId query int false "department id"
 // @Param userId query int true "deleted user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
@@ -76,10 +114,28 @@ func (H *Handler) deleteDepartmentAdmin(c *gin.Context) {
 // @Summary NOT IMPLEMENTED! DepartmentMaintainer
 // @Security ApiKeyAuth
 // @Tags rights
+// @Description This request for get users from department maintainers list
+// @ID getDepartmentMaintainers
+// @Accept json
+// @Produces json
+// @Param departmentId query int false "department id"
+// @Success 200 {array} core.DepartmentManager "list of department maintainer"
+// @Failure 400,401,403 {object} errorResponse
+// @Failure 500 {object} errorResponse
+// @Failure default {object} errorResponse
+// @Router /department_maintainers [get]
+func (H *Handler) getDepartmentMaintainers(c *gin.Context) {
+	newErrorResponse(c, http.StatusInternalServerError, "method not implemented")
+}
+
+// @Summary NOT IMPLEMENTED! DepartmentMaintainer
+// @Security ApiKeyAuth
+// @Tags rights
 // @Description This request for add user to department maintainers
 // @ID addDepartmentMaintainer
 // @Accept json
 // @Produces json
+// @Param departmentId query int false "department id"
 // @Param userId query int true "added user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
@@ -97,6 +153,7 @@ func (H *Handler) addDepartmentMaintainer(c *gin.Context) {
 // @ID deleteDepartmentMaintainer
 // @Accept json
 // @Produces json
+// @Param departmentId query int false "department id"
 // @Param userId query int true "deleted user id"
 // @Success 200 {string} string "ok"
 // @Failure 400,401,403 {object} errorResponse
