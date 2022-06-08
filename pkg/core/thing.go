8d8df4adf2ea671d2f73c9cc1a63c42962386c67
emil.islamov@mircod.com <S568gKs5>
2022-06-08 18:35:45 +0300

    Fixed thing structs

--- pkg/core/thing.go
+++ pkg/core/thing.go
@@ -5,9 +5,6 @@ type ThingBase struct {
 	Type              string  `json:"type" binding:"required"`
 	Remainder         float32 `json:"remainder"`
 	RemainderType     string  `json:"remainder_type"`
-	IsBlocked         bool    `json:"is_blocked"`
-	BlockedTimeStart  int64   `json:"blocked_time_start"`
-	BlockedTimeEnd    int64   `json:"blocked_time_end"`
 	NeedAdminApproval bool    `json:"need_admin_approval"`
 }
 
@@ -17,4 +14,32 @@ type Thing struct {
 	ImageURL     string `json:"image_url"`
 	CompanyId    int    `json:"company_id"`
 	DepartmentId int    `json:"department_id"`
+}
+
+type ThingActionBase struct {
+	UserId    int    `json:"user_id" binding:"required"`
+	ThingId   int    `json:"thing_id" binding:"required"`
+	StartTime uint32 `json:"start_time" binding:"required"`
+	EndTime   uint32 `json:"end_time"`
+}
+
+type ThingBlockAdd struct {
+	ThingActionBase
+	Reason string `json:"reason" binding:"required"`
+}
+
+type ThingBlock struct {
+	ThingBlockAdd
+	Id int `json:"id"`
+}
+
+type ThingUsageAdd struct {
+	ThingActionBase
+}
+
+type ThingUsage struct {
+	ThingUsageAdd
+	Id         int  `json:"id"`
+	IsApproved bool `json:"is_approved"`
+	IsTaken    bool `json:"is_taken"`
 }
