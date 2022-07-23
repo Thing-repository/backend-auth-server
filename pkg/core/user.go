8575ff9df2df4eb57fb5aa97a5a9fd7317cea5f8
Emil <emil.islmaov@mircod.com>
2022-07-23 21:39:06 +0300

    make all fields in user struct as pointer for update userDB function

--- pkg/core/user.go
+++ pkg/core/user.go
@@ -1,9 +1,9 @@
 package core
 
 type UserBaseData struct {
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-	Email     string `json:"email" binding:"required"`
+	FirstName *string `json:"first_name" binding:"required"`
+	LastName  *string `json:"last_name" binding:"required"`
+	Email     *string `json:"email" binding:"required"`
 }
 
 type UserChange struct {
@@ -14,8 +14,8 @@ type UserChange struct {
 
 type User struct {
 	UserChange
-	EmailIsValidated bool    `json:"email_is_validated"`
-	Id               int     `json:"id" binding:"required"`
+	EmailIsValidated *bool   `json:"email_is_validated"`
+	Id               *int    `json:"id" binding:"required"`
 	ImageURL         *string `json:"image_url,omitempty"`
 	CompanyId        *int    `json:"company_id,omitempty"`
 	DepartmentId     *int    `json:"department_id,omitempty"`
@@ -23,8 +23,8 @@ type User struct {
 
 type UserDB struct {
 	User
-	PasswordHash         string `json:"password_hash"`
-	EmailValidationToken string `json:"email_validation_token"`
+	PasswordHash         *string `json:"password_hash"`
+	EmailValidationToken *string `json:"email_validation_token"`
 }
 
 type AddUserDB struct {
