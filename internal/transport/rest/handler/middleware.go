98fe0ee728df763dce1cf7e945c7c6bcaacc1c1a
Emil <emil.islmaov@mircod.com>
2022-07-26 09:11:58 +0300

    add plan

--- internal/transport/rest/handler/middleware.go
+++ internal/transport/rest/handler/middleware.go
@@ -13,6 +13,7 @@ const (
 )
 
 func (H *Handler) userIdentity(c *gin.Context) {
+	// TODO: realise check permission
 	header := c.GetHeader(authorizationHeader)
 	if header == "" {
 		newErrorResponse(c, http.StatusUnauthorized, "empty authorization header")
