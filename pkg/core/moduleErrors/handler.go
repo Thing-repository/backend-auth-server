6db1635398593bb15665bae1c7ad90e50e6d6c87
Emil <emil.islmaov@mircod.com>
2022-07-26 09:11:30 +0300

    add more errors

--- pkg/core/moduleErrors/handler.go
+++ pkg/core/moduleErrors/handler.go
@@ -6,4 +6,6 @@ var (
 	ErrorHandlerInvalidUsernameOrPassword = errors.New("invalid username or password")
 	ErrorHandlerInvalidEmail              = errors.New("invalid email address")
 	ErrorHandlerUserAlreadyHasCompany     = errors.New("user already has company")
+	ErrorNoRequiredFieldsQuery            = errors.New("no required Fields in query")
+	ErrorForbidden                        = errors.New("you have not access to this resource")
 )
