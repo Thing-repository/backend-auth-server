554eb59cc95d88a48e195368c7a6fcc1425833dc
Emil <emil.islmaov@mircod.com>
2022-08-05 10:57:33 +0300

    update service errors

--- pkg/core/moduleErrors/service.go
+++ pkg/core/moduleErrors/service.go
@@ -3,7 +3,11 @@ package moduleErrors
 import "errors"
 
 var (
-	ErrorServiceUserNotFound    = errors.New("user not found")
-	ErrorServiceInvalidPassword = errors.New("invalid password")
-	ErrorServiceUserAlreadyHas  = errors.New("there is already a user with this email")
+	ErrorServiceUserNotFound          = errors.New("user not found")
+	ErrorServiceInvalidPassword       = errors.New("invalid password")
+	ErrorServiceUserAlreadyHas        = errors.New("there is already a user with this email")
+	ErrorServiceGetUserData           = errors.New("error get user data")
+	ErrorServiceUserAlreadyHasCompany = errors.New("user already has company")
+	ErrorServiceBadPermissions        = errors.New("access denied")
+	ErrorServiceInvalidContext        = errors.New("invalid context")
 )
