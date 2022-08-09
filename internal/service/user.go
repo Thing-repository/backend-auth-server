3b9d66782b17639b2676b3420b9dc368e09ea6bf
Emil <emil.islmaov@mircod.com>
2022-08-09 09:46:42 +0300

    add function for add user to company, update required dependencies

--- internal/service/user.go
+++ internal/service/user.go
@@ -4,6 +4,7 @@ import "C"
 import (
 	"context"
 	"github.com/Thing-repository/backend-server/pkg/core"
+	"github.com/Thing-repository/backend-server/pkg/core/moduleErrors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -11,6 +12,14 @@ type userDBUser interface {
 	PathUser(ctx context.Context, user *core.UserDB, userId int) error
 	GetUser(ctx context.Context, userId int) (*core.UserDB, error)
 	GetUsersFilter(ctx context.Context, filter string, limit int, offset int) ([]core.User, error)
+}
+
+type departmentDBUser interface {
+	GetDepartment(ctx context.Context, departmentId int) (*core.Department, error)
+}
+
+type credentialsDBUser interface {
+	CreateCredential(ctx context.Context, credentials *core.AddCredentials) (int, error)
 }
 
 type transactionDBUser interface {
@@ -22,12 +31,16 @@ type transactionDBUser interface {
 
 type User struct {
 	userDB        userDBUser
+	departmentDB  departmentDBUser
+	credentialsDB credentialsDBUser
 	transactionDB transactionDBUser
 }
 
-func NewUser(userDB userDBUser, transactionDB transactionDBUser) *User {
+func NewUser(userDB userDBUser, departmentDB departmentDBUser, credentialsDB credentialsDBUser, transactionDB transactionDBUser) *User {
 	return &User{
 		userDB:        userDB,
+		departmentDB:  departmentDB,
+		credentialsDB: credentialsDB,
 		transactionDB: transactionDB,
 	}
 }
@@ -52,4 +65,122 @@ func (U *User) FindUsersForInvite(ctx context.Context, filter string, limit int,
 	}
 
 	return users, nil
+}
+
+func (U *User) AddUserToCompany(ctx context.Context, userId int, departmentId int) error {
+	logBase := logrus.Fields{
+		"module":       "user",
+		"function":     "AddUserToCompany",
+		"userId":       userId,
+		"departmentId": departmentId,
+		"context":      *core.LogContext(ctx),
+	}
+	ctx, err := U.transactionDB.InjectTx(ctx)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":  logBase,
+			"error": err.Error(),
+		}).Error("error create transaction")
+		return err
+	}
+
+	defer U.transactionDB.RollbackTxDefer(ctx)
+
+	credentials, err := core.ContextGetUserCredentials(ctx)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":  logBase,
+			"error": err.Error(),
+		}).Error("error get user credentials from context")
+		return moduleErrors.ErrorServiceInvalidContext
+	}
+
+	departmentData, err := U.departmentDB.GetDepartment(ctx, departmentId)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":         logBase,
+			"departmentId": departmentId,
+			"error":        err.Error(),
+		}).Error("error get department data from db")
+		return err
+	}
+
+	// TODO: add check rights for service admin, load and check permission form context
+	isCompanyAdmin := core.CheckCredential(credentials, core.CredentialTypeCompanyAdmin, *departmentData.CompanyId)
+	isDepartmentAdmin := core.CheckCredential(credentials, core.CredentialTypeDepartmentAdmin, departmentId)
+
+	if !isCompanyAdmin || !isDepartmentAdmin {
+		logrus.WithFields(logrus.Fields{
+			"base":         logBase,
+			"companyId":    departmentData.CompanyId,
+			"departmentId": departmentId,
+		}).Error("user has not credentials")
+		return moduleErrors.ErrorServiceBadPermissions
+	}
+
+	userData, err := U.userDB.GetUser(ctx, userId)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":   logBase,
+			"userId": userId,
+			"error":  err.Error(),
+		}).Error("error get user data from db")
+		return err
+	}
+	if userData.CompanyId != nil || userData.DepartmentId != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":   logBase,
+			"userId": userId,
+		}).Error("user already has company")
+		return moduleErrors.ErrorServiceUserAlreadyHasCompany
+	}
+
+	newUserData := core.UserDB{
+		User: core.User{
+			CompanyId:    departmentData.CompanyId,
+			DepartmentId: &departmentId,
+		},
+	}
+	err = U.userDB.PathUser(ctx, &newUserData, userId)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":        logBase,
+			"userId":      userId,
+			"newUserData": newUserData,
+			"error":       err.Error(),
+		}).Error("error update user data in db")
+		return err
+	}
+
+	_, err = U.credentialsDB.CreateCredential(ctx, newCredential(departmentId, userId, core.CredentialTypeDepartmentUser))
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":         logBase,
+			"departmentId": departmentId,
+			"userId":       userId,
+			"error":        err.Error(),
+		}).Error("error add department admin to database")
+		return err
+	}
+
+	_, err = U.credentialsDB.CreateCredential(ctx, newCredential(*departmentData.CompanyId, userId, core.CredentialTypeCompanyUser))
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":      logBase,
+			"companyId": departmentData.CompanyId,
+			"userId":    userId,
+			"error":     err.Error(),
+		}).Error("error add department admin to database")
+		return err
+	}
+
+	if err = U.transactionDB.CommitTx(ctx); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"base":  logBase,
+			"error": err.Error(),
+		}).Error("error commit transaction")
+		return err
+	}
+
+	return nil
 }
