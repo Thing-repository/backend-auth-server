c0c1f9ff720165c9aae012aa5b4f3328a7682219
Emil <emil.islmaov@mircod.com>
2022-08-09 09:43:47 +0300

    change department structs to pointers

--- pkg/core/department.go
+++ pkg/core/department.go
@@ -1,12 +1,12 @@
 package core
 
 type DepartmentBase struct {
-	DepartmentName string `json:"department_name" binding:"required"`
-	CompanyId      int    `json:"company_id" binding:"required"`
+	DepartmentName *string `json:"department_name" binding:"required"`
+	CompanyId      *int    `json:"company_id" binding:"required"`
 }
 
 type Department struct {
 	DepartmentBase
-	Id       int    `json:"id"`
-	ImageURL string `json:"image_url"`
+	Id       *int    `json:"id"`
+	ImageURL *string `json:"image_url"`
 }
